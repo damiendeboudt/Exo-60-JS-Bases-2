f0345eea11588a71c17e648a1ab5f562f4e6b14f
Damien <damien.deboudt@sfr.fr>
2022-11-20 17:42:09 +0100

    ok

---
+++ exo 1/script.js
@@ -0,0 +1,11 @@
+let item = document.getElementsByTagName("li");
+for(let i = 0; i < item.length; i++) {
+    item[i].innerHTML = "blabla";
+
+}
+let list = document.getElementsByTagName("ul");
+ for (let i = 0; i < list.length; i++) {
+     list[i].style.listStyle = "circle";
+ }
+
+
